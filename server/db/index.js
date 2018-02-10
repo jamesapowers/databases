6885b9b990a4c7b91e28acf12f6c294f2406b3f0
jamesapowers <powersajames@gmail.com>
2018-02-09 20:16:30 -0800

    Implement working mySQL database

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,19 @@
 var mysql = require('mysql');
 
+var dbConnection = mysql.createConnection({
+  user: 'student',
+  password: 'student',
+  database: 'chat'
+});
+dbConnection.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log('database connected');
+});
+  
+
+exports.dbConnection;
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
