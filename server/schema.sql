3a1fed9bf279d38560be19eb466adee9b5192b5a
jamesapowers <powersajames@gmail.com>
2018-02-09 16:30:05 -0800

    Frame out our data structure

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,28 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  MessageID int NOT NULL,
+  PRIMARY KEY (MessageID),
+  FOREIGN KEY (UserID) REFERENCES users(UserID),
+  FOREIGN KEY (RoomID) REFERENCES rooms(RoomID),
+  message VARCHAR(140),
 );
+
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  UserID INT NOT NULL,
+  PRIMARY KEY (UserID),
+  username VARCHAR(30),
 
+);
+
+CREATE TABLE rooms (
+  RoomID INT NOT NULL,
+  PRIMARY KEY (RoomID),
+  roomname VARCHAR(30),
+);
 
 
 /*  Execute this file from the command line by typing:
