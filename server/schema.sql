8f70dc7426733dae7ba421ed3b2a2505fb82b769
jamesapowers <powersajames@gmail.com>
2018-02-09 20:15:03 -0800

    Implements working mySQL database

--- server/schema.sql
+++ server/schema.sql
@@ -2,33 +2,35 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-  MessageID int NOT NULL,
-  PRIMARY KEY (MessageID),
-  FOREIGN KEY (UserID) REFERENCES users(UserID),
-  FOREIGN KEY (RoomID) REFERENCES rooms(RoomID),
-  message VARCHAR(140),
-);
 
 
 /* Create other tables and define schemas for them here! */
 
 CREATE TABLE users (
-  UserID INT NOT NULL,
+  UserID INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (UserID),
-  username VARCHAR(30),
+  username VARCHAR(30)
 
 );
 
 CREATE TABLE rooms (
-  RoomID INT NOT NULL,
+  RoomID INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (RoomID),
-  roomname VARCHAR(30),
+  roomname VARCHAR(30)
+);
+  /* Describe your table here.*/
+
+CREATE TABLE messages (
+  MessageID int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (MessageID),
+  UserID int, 
+  RoomID int,
+  FOREIGN KEY (UserID) REFERENCES users(UserID),
+  FOREIGN KEY (RoomID) REFERENCES rooms(RoomID),
+  message VARCHAR(140)
 );
 
-
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
